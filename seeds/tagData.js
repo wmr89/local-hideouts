6841f1edfa65b8389ba3aa45df9458d1875b6f33
williamrankin <william.rankin01@gmail.com>
2024-02-08 19:57:21 -0700

    added seed data

--- seeds/tagData.js
+++ seeds/tagData.js
@@ -3,6 +3,9 @@ const { Tag } = require("../models");
 const tagData = [
     { name: "Relaxed Atmosphere" },
     { name: "Authentic" },
+    { name: "middle-of-nowhere"},
+    { name: "music"},
+    
 ];
 
 const seedTags = () => Tag.bulkCreate(tagData);
